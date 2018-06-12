30207b56f348e3ea8f657e6df1e9399a7a6c5a2b
morita-jun <morita-jun@dmm.com>
2018-06-12 15:43:42 +0900

    Initial Commit

---
+++ MVVMSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMSample
+//
+//  Created by morita-jun on 2018/06/12.
+//  Copyright © 2018年 morita-jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
